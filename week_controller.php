885d4f4d8c7557c23d68ebe2acbf86f7c5055710
Pablo A.  Silva <pablodeas@gmail.com>
2023-04-07 10:51:41 -0300

    Adjust to show available balance

--- week_controller.php
+++ week_controller.php
@@ -14,27 +14,32 @@ date_default_timezone_set('America/Sao_Paulo')
 </head>
 
 <body>
-
+    <!--Título da Página-->
     <div class="d-flex justify-content-center h4">Controle Semanal &#9749;</div>
     <br>
+
+    <!--Variáveis de data atual e próximo domingo-->
     <?php
     $proximo_domingo = strtotime('next Sunday');
     $date = date('d-m-Y', $proximo_domingo);
     ?>
 
+    <!--Texto básico-->
     <p class="d-flex justify-content-center">Hoje é o dia:</p>
     <p class="d-flex justify-content-center"><strong><?php echo date('d-m-Y'); ?></strong> </p>
     <p class="d-flex justify-content-center">A semana se encerrá no dia:</p>
     <p class="d-flex justify-content-center"><strong><?php echo $date; ?></strong> </p>
 
-    <form action="/week_finan_controller.php" method="post">
+    <!--Formulário que recebe o valor gasto no dia-->
+    <form action="/week_controller.php" method="post">
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label d-flex justify-content-center">Digite o gasto:</label>
             <input type="number" class="form-control" id="exampleInputNumber1" name="value">
         </div>
     </form>
 
-    <div class="d-flex justify-content-center">Total:</div>
+    <!--Parte do cálculo-->
+    <div class="d-flex justify-content-center">Você gastou até o momento:</div>
     <div class="d-flex justify-content-center">
         <?php
         session_start();
@@ -54,8 +59,15 @@ date_default_timezone_set('America/Sao_Paulo')
         echo "R$" . $_SESSION['total']; 
         ?>
     </div>
-
-
+    <div class="d-flex justify-content-center">Ainda tem disponível:</div>
+    <div class="d-flex justify-content-center">
+        <?php
+        $limite = 120;
+        $disponível = $limite - $_SESSION['total'];
+        
+        echo "R$" . $disponível;
+        ?>
+    </div>
 </body>
 
 </html>
